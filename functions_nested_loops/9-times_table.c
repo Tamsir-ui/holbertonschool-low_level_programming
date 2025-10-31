ae78295eb6581f136dce35e53ce9a76a32e5e7e4
Padawan <djibriltamsirn@gmail.com>
2025-10-31 10:14:04 +0100

    Refactor times_table function for clarity

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -5,30 +5,30 @@
  */
 void times_table(void)
 {
-int i, j, prod;
+  int i, j, prod;
 
-for (i = 0; i <= 9; i++)
-{
-for (j = 0; j <= 9; j++)
-{
-prod = i * j;
-if (j == 0)
-_putchar(prod + '0');
-else if (prod < 10)
-{
-_putchar(',');
-_putchar(' ');
-_putchar(' ');
-_putchar(prod + '0');
-}
-else
-{
-_putchar(',');
-_putchar(' ');
-_putchar((prod / 10) + '0');
-_putchar((prod % 10) + '0');
-}
-}
-_putchar('\n');
-}
+  for (i = 0; i <= 9; i++)
+  {
+    for (j = 0; j <= 9; j++)
+    {
+      prod = i * j;
+      if (j == 0)
+        _putchar('0');
+      else if (prod < 10)
+      {
+        _putchar(',');
+        _putchar(' ');
+        _putchar(' ');
+        _putchar(prod + '0');
+      }
+      else
+      {
+      _putchar(',');
+      _putchar(' ');
+      _putchar((prod / 10) + '0');
+      _putchar((prod % 10) + '0');
+      }
+    }
+    _putchar('\n');
+  }
 }
