41c93e73edabe66bad80d2f3199fec571d9e35c7
Djibril <djibriltamsirn@gmail.com>
2025-11-03 10:46:53 +0000

    add 4-print_rev.c

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -5,13 +5,15 @@
 *@s: string
 */
 void print_rev(char *s)
+
 {
 int len = 0;
-
+ int i;
+  
 while (s[len] != '\0')
-len--;
+len++;
 
-for (len = len - 1; len >= 0; len--)
-_putchar(s[len]);
+for (i = len - 1; i >= 0; i--)
+_putchar(s[i]);
 _putchar('\n');
 }
