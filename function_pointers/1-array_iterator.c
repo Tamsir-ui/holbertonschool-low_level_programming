303932d0204be9b1810d2a7aa3a3593c443d17af
Padawan <djibriltamsirn@gmail.com>
2025-11-20 11:26:23 +0100

    Refactor array_iterator function for clarity

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -1,5 +1,5 @@
 #include "function_pointers.h"
-
+#include <stdio.h>
 /**
  * array_iterator - executes a function given as a parameter
  *                  on each element of an array
@@ -11,11 +11,9 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-size_t i;
-
-if (array == NULL || action == NULL)
-return;
-
-for (i = 0; i < size; i++)
-action(array[i]);
+	size_t i;
+	if (array == NULL || action == NULL)
+		return;
+	for (i = 0; i < size; i++)
+		action(array[i]);
 }
