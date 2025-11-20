cf460306e95ee0ece2fb8fd971ca9cc492ee34c0
Padawan <djibriltamsirn@gmail.com>
2025-11-20 11:28:53 +0100

    Update 2-int_index.c

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -1,5 +1,5 @@
 #include "function_pointers.h"
-
+#include <stdio.h>
 /**
  * int_index - searches for an integer
  * @array: array to search
@@ -10,16 +10,14 @@
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-int i;
+	int i;
 
-if (array == NULL || cmp == NULL || size <= 0)
-return (-1);
-
-for (i = 0; i < size; i++)
-{
-if (cmp(array[i]) != 0)
-return (i);
-}
-
-return (-1);
+	if (array == NULL || cmp == NULL || size <= 0)
+		return (-1);
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]) != 0)
+			return (i);
+	}
+	return (-1);
 }
