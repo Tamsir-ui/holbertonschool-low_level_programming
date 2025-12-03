5a02f3eeea01f5a75251c34d7028b1d5fbaeb80a
Tamsir-ui <djibriltamsirn@gmail.com>
2025-12-03 11:13:08 +0100

    feat: adding tasks 1-2-3-4 of singly_linked_lists project

---
+++ singly_linked_lists/2-add_node.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+
+/**
+ * add_node - adds a new node at the beginning of a list_t list
+ * @head: pointer to the pointer to the head
+ * @str: string to duplicate
+ *
+ * Return: address of the new element, or NULL if failure
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new;
+	unsigned int len = 0;
+
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+
+	new->str = strdup(str);
+	if (new->str == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
+
+	while (str[len] != '\0')
+		len++;
+
+	new->len = len;
+
+	new->next = *head;
+	*head = new;
+
+	return (new);
+}
